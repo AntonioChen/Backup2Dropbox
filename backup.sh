7f6b7d819d87153275110d36b7935b0c3e4d5d44
AntonioChen <chenxuanyi1991@gmail.com>
2013-02-13 16:59:54 +0800

    2 files

---
+++ backup.sh
@@ -0,0 +1,60 @@
+#---------------DON'T EDIT---------------
+BACKUP_DST="/tmp"		#tmp folder
+
+WEEK=$(date +"%A")
+NOW=$(date +"%Y-%m-%d_%H-%M-%S")
+DESTFILE="$BACKUP_DST/$NOW.tar.gz"
+TARFILE=""
+
+# Select file
+if [ $BACKUP_FILE -eq 1 ]
+then
+	echo "Select file."
+	TARFILE=$FILE_SRC
+else
+	echo "Skip selecting file."
+fi
+
+# Mysql
+if [ $BACKUP_MYSQL -eq 1 ]
+then
+	echo "Back up mysql."
+	mysqldump -u $MYSQL_USER -h $MYSQL_SERVER -p$MYSQL_PASS --all-databases > "$NOW-Databases.sql"
+	TARFILE=$TARFILE" $NOW-Databases.sql"
+else
+	echo "Skip backing up mysql."
+fi
+
+# Backup crontab
+if [ $BACKUP_CRON -eq 1 ]
+then
+	echo "Back up crontab."
+	crontab -l >> "$NOW-crontab.txt"
+	TARFILE=$TARFILE" $NOW-crontab.txt"
+else
+	echo "Skip Backing up crontab."
+fi
+
+# Tar all files
+if [ $BACKUP_FILE -eq 1 -o $BACKUP_MYSQL -eq 1 -o $BACKUP_CRON -eq 1 ]
+then
+	echo "Tar all files: $TARFILE"
+	tar cfz "$DESTFILE" $TARFILE
+
+	#Delete old files
+	echo "Delete old files"
+	$SCRIPT_DIR/dropbox_uploader.sh delete "$DROPBOX_DIR/$WEEK"
+
+	# Upload
+	echo "Upload to dropbox"
+	$SCRIPT_DIR/dropbox_uploader.sh upload "$DESTFILE" "$DROPBOX_DIR/$WEEK/$NOW.tar.gz"
+
+	# Delete local tmp file
+	echo "Delete local tmp file"
+	echo "rm -f $NOW-Databases.sql $NOW-crontab.txt $DESTFILE"
+	rm -f "$NOW-Databases.sql $NOW-crontab.txt $DESTFILE"
+else
+	echo "No files to upload"
+fi
+
+echo "backup2Dropbox finished."
--- backup2Dropbox.sh
+++ backup2Dropbox.sh
@@ -4,7 +4,7 @@
 # wget --no-check-certificate https://raw.github.com/AntonioChen/Backup2Dropbox/master/backup2Dropbox.sh
 
 SCRIPT_DIR="/root/backup"	#The folder of dropbox_uploader.sh
-DROPBOX_DIR="/"			#The folder in Dropbox
+DROPBOX_DIR=""			#The folder in Dropbox
 
 BACKUP_FILE=0
 FILE_SRC="/svn /www"		#The folder to backup, using blank between different paths
@@ -15,62 +15,19 @@ MYSQL_USER="root"
 MYSQL_PASS="123456"
 
 BACKUP_CRON=1
+
 #---------------DON'T EDIT---------------
-BACKUP_DST="/tmp"		#tmp folder
+export SCRIPT_DIR
+export DROPBOX_DIR
 
-WEEK=$(date +"%A")
-NOW=$(date +"%Y-%m-%d_%H-%M-%S")
-DESTFILE="$BACKUP_DST/$NOW.tar.gz"
-TARFILE=""
+export BACKUP_FILE
+export FILE_SRC
 
-# Select file
-if [ $BACKUP_FILE -eq 1 ]
-then
-	echo "Select file."
-	TARFILE=$FILE_SRC
-else
-	echo "Skip selecting file."
-fi
+export BACKUP_MYSQL
+export MYSQL_SERVER
+export MYSQL_USER
+export MYSQL_PASS
 
-# Mysql
-if [ $BACKUP_MYSQL -eq 1 ]
-then
-	echo "Back up mysql."
-	mysqldump -u $MYSQL_USER -h $MYSQL_SERVER -p$MYSQL_PASS --all-databases > "$NOW-Databases.sql"
-	TARFILE=$TARFILE" $NOW-Databases.sql"
-else
-	echo "Skip backing up mysql."
-fi
+export BACKUP_CRON
 
-# Backup crontab
-if [ $BACKUP_CRON -eq 1 ]
-then
-	echo "Back up crontab."
-	crontab -l >> "$NOW-crontab.txt"
-	TARFILE=$TARFILE" $NOW-crontab.txt"
-else
-	echo "Skip Backing up crontab."
-fi
-
-# Tar all files
-if [ $BACKUP_FILE -eq 1 -o $BACKUP_MYSQL -eq 1 -o $BACKUP_CRON -eq 1 ]
-then
-	echo "Tar all files: $TARFILE"
-	tar cfz "$DESTFILE" $TARFILE
-
-	#Delete old files
-	echo "Delete old files"
-	$SCRIPT_DIR/dropbox_uploader.sh delete "$DROPBOX_DIR/$WEEK"
-
-	# Upload
-	echo "Upload to dropbox"
-	$SCRIPT_DIR/dropbox_uploader.sh upload "$DESTFILE" "$DROPBOX_DIR/$WEEK/$NOW.tar.gz"
-
-	# Delete local tmp file
-	echo "Delete local tmp file"
-	echo "rm -f $NOW-Databases.sql $NOW-crontab.txt $DESTFILE"
-	rm -f "$NOW-Databases.sql $NOW-crontab.txt $DESTFILE"
-else
-	echo "No files to upload"
-fi
-echo "backup2Dropbox finished."
+$SCRIPT_DIR/backup.sh
